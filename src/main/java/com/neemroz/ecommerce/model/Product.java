8b9de7b8259c2e275cb30a47bb1f791adc4144c1
Md Shayan Moazzam <shayanhaldia@gmail.com>
2024-08-29 13:52:37 +0530

    commit 2

--- src/main/java/com/neemroz/ecommerce/model/Product.java
+++ src/main/java/com/neemroz/ecommerce/model/Product.java
@@ -17,6 +17,98 @@ public class Product {
     private String description;
     private double price;
 
+    private String imageUrl;
+
+    private String setContent;
+    private String material;
+    private String bedsheetSize;
+    private String pillowCoverSize;
+    private String features;
+    private String fit;
+    private String threadCount;
+    private String care;
+    private String shippingWeight;
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getSetContent() {
+        return setContent;
+    }
+
+    public void setSetContent(String setContent) {
+        this.setContent = setContent;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public String getBedsheetSize() {
+        return bedsheetSize;
+    }
+
+    public void setBedsheetSize(String bedsheetSize) {
+        this.bedsheetSize = bedsheetSize;
+    }
+
+    public String getPillowCoverSize() {
+        return pillowCoverSize;
+    }
+
+    public void setPillowCoverSize(String pillowCoverSize) {
+        this.pillowCoverSize = pillowCoverSize;
+    }
+
+    public String getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(String features) {
+        this.features = features;
+    }
+
+    public String getFit() {
+        return fit;
+    }
+
+    public void setFit(String fit) {
+        this.fit = fit;
+    }
+
+    public String getThreadCount() {
+        return threadCount;
+    }
+
+    public void setThreadCount(String threadCount) {
+        this.threadCount = threadCount;
+    }
+
+    public String getCare() {
+        return care;
+    }
+
+    public void setCare(String care) {
+        this.care = care;
+    }
+
+    public String getShippingWeight() {
+        return shippingWeight;
+    }
+
+    public void setShippingWeight(String shippingWeight) {
+        this.shippingWeight = shippingWeight;
+    }
+
     // Getters and Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
